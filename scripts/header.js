539a4f2a4ddeec0af8fdd4975bcfebc59e0fd2ff
Atuldubey98 <atuldubey017@gmail.com>
2025-03-21 17:05:03 +0530

    fix : ds polychem products

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,12 @@
 const headerLinksList = document.getElementsByClassName("header__link");
 const currentLocation = window.origin + window.location.pathname;
+let found = false;
 for (const headerLink of headerLinksList) {
   if (headerLink.href === currentLocation) {
     headerLink.classList.add("header__linkSelected");
+    found = true;
+  }
+  if (!found) {
+    headerLinksList[0].classList.add("header__linkSelected");
   }
 }
